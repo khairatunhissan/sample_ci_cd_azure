037ec653c25aa68d6c69e9476f9c2e608e4a82d3
khairatunhissan <khairatunhissan@gmail.com>
2021-12-08 12:33:07 +0600

    Set up

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send("Welcome to Azure")
+})
+
+app.listen(8080, ()=> {
+    console.log('Server is listening to port 8080');
+})
